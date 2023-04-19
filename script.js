82c0d9d3a9189f936d9d9315bc0c4b3723600d67
Shahriyar Kashif <shahriyar.k@hotmail.com>
2023-04-19 08:31:05 +0500

    code reviewed and cleaned for all three files

--- script.js
+++ script.js
@@ -26,25 +26,19 @@ const apiKey = '19088892fbf842ec94bf493e139bb3af';
 const cardBox = document.querySelector('.card-container');
 const form = document.querySelector('form');
 
-let resultsArray;
 fetch(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=6`)
-.then(response => {
-    handleError(response);
-    return response.json()
-})
+.then(response => handleError(response))
 .then(data => data.recipes)
 .then(array => {
     document.querySelector('.search-title').innerText = `Popular Recipes`;
-    let ingredients = [];
     array.forEach(element => {
         let ingrPara = addCards(element);
-        element.extendedIngredients.forEach(element => {
-            ingredients.push(element.name);
-        })
-        ingrPara.innerText = ingredients.join(', ');
+        let ingredientsList = getIngredients(element.extendedIngredients);
+        ingrPara.innerText = ingredientsList.join(', ');
     })
 });
 
+let resultsArray;
 form.addEventListener('submit', (event)=>{
     resultsArray = [];
     document.querySelector('.recipe-details').style.display = 'none';
@@ -52,19 +46,14 @@ form.addEventListener('submit', (event)=>{
     document.querySelector('.search-title').style.display = 'block';
     document.querySelector('#recipe-info-title').style.display = 'none';
     event.preventDefault();
-    // document.querySelector('form h1').style.display = 'none';
-    // document.querySelector('form p').style.display = 'none';
     const cardDivs = document.querySelectorAll('.card');
     removePrevious(cardDivs);
     let query = search.value;
-    // document.querySelector('.search-title').innerText = '';
-    // enableSearch(query);
-    let checkLocal = checkCache(query);
+    let checkLocal = checkLocalStorage(query);
     if (checkLocal) {
-        console.log("Yayyyy")
         let localArray = JSON.parse(localStorage.getItem(checkLocal));
         let count = localArray.length;
-        document.querySelector('.search-title').innerText = `${query} (${count} results)`
+        document.querySelector('.search-title').innerText = `${query}-${count} result(s)`
         localArray.forEach(element => {
             let ingrPara = addCards(element);
             let ingredients = element.ingredients;
@@ -73,32 +62,22 @@ form.addEventListener('submit', (event)=>{
     }
     else {
         fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`)
-        .then(response => {
-            handleError(response);
-            return response.json()
-        })
+        .then(response => handleError(response))
         .then(data => data.results)
         .then(array => {
-            console.log('BOOOO')
             let count = array.length;
-            document.querySelector('.search-title').innerText = `${query} (${count} result(s))`
+            document.querySelector('.search-title').innerText = `${query}-${count} result(s)`
             array.forEach(element => {
                 let ingrPara = addCards(element);
                 let recipeId = element.id;
                 let imgSource = element.image;
                 fetch(`https://api.spoonacular.com/recipes/${recipeId}/ingredientWidget.json?apiKey=${apiKey}`)
-                .then(response => {
-                    handleError(response);
-                    return response.json()
-                })
+                .then(response => handleError(response))
                 .then(data => data.ingredients)
                 .then(array => {
-                    let ingredients = [];
-                    array.forEach(object => {
-                        ingredients.push(object.name);
-                    })
-                    ingrPara.innerText = ingredients.join(', ')
-                    let foodItem = {image:imgSource, title:element.title, ingredients:ingredients, id:recipeId}
+                    let ingredientsList = getIngredients(array);
+                    ingrPara.innerText = ingredientsList.join(', ');
+                    let foodItem = {image:imgSource, title:element.title, ingredients:ingredientsList, id:recipeId}
                     resultsArray.push(foodItem);
                     localStorage.setItem(query, JSON.stringify(resultsArray));
                 })
@@ -115,43 +94,7 @@ function removePrevious(elmCollection) {
     }
 }
 
-function showRecipe(event) {
-    let elm = event.target;
-    let paraIngr = elm.previousElementSibling;
-    let headIngr = paraIngr.previousElementSibling;
-    let paraTitle = headIngr.previousElementSibling;
-    let cardDiv = elm.parentElement;
-    let recipeId = cardDiv.getAttribute('value');
-    document.querySelector('.search-title').style.display = 'none';
-    document.getElementById('back-btn').style.display = 'block';
-    document.getElementById('back-btn').addEventListener('click', () => {
-        cardBox.style.display = 'flex';
-        document.getElementById('back-btn').style.display = 'none';
-        document.querySelector('.recipe-details').style.display = 'none';
-        document.querySelector('#recipe-info-title').style.display = 'none'
-        document.querySelector('.search-title').style.display = 'block';
-    })
-    fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`)
-    .then(response => {
-        handleError(response);
-        return response.json()
-    })
-    .then(data => {
-        document.querySelector('.recipe-details').style.display = 'block';
-        document.querySelector('.card-container').style.display = 'none';
-        // document.querySelector('.search-title').style.display = 'none';
-        document.getElementById('summary').innerHTML = data.summary;
-        document.querySelector('.recipe-details img').setAttribute('src',data.image);
-        document.getElementById('instructions').innerHTML = data.instructions;
-        document.getElementById('ingredients').innerText = paraIngr.innerText;
-        document.querySelector('#recipe-info-title').style.display = 'block';
-        // document.getElementById('title').innerText = paraTitle.innerText;
-        document.querySelector('#recipe-info-title').innerText = paraTitle.innerText;
-    })
-}
-
-
-function checkCache(search) {
+function checkLocalStorage(search) {
     for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
         if (search.toLowerCase() == key.toLowerCase()) {
@@ -161,38 +104,67 @@ function checkCache(search) {
     return false;
 }
 
-
 function addCards (object) {
     let imgSource = object.image;
     let recipeId = object.id;
     let card = document.createElement('div');
-
     document.querySelector('.card-container').append(card);
+    card.setAttribute('value', recipeId);
     card.setAttribute('class','card')
-
     let image = document.createElement('img');
     card.append(image);
+    image.setAttribute('src', imgSource)
     let nameSpan = document.createElement('span');
-    card.setAttribute('value', recipeId);
     card.append(nameSpan);
+    nameSpan.innerText = 'Recipe Name';
     let namePara = document.createElement('p');
     card.append(namePara);
+    namePara.innerText = object.title;
     let ingrSpan = document.createElement('span');
     card.append(ingrSpan);
+    ingrSpan.innerText = 'Ingredients';
     let ingrPara = document.createElement('p');
     card.append(ingrPara);
     let button = document.createElement('button');
     card.append(button);
     button.setAttribute('class', 'btn');
-    image.setAttribute('src', imgSource)
-    button.innerText = 'Click for more information'
-    nameSpan.innerText = 'Recipe Name'
-    ingrSpan.innerText = 'Ingredients'
-    namePara.innerText = object.title;
+    button.innerText = 'Click for more information';
     button.addEventListener('click', showRecipe);
     return ingrPara;
 }
 
+function showRecipe(event) {
+    let elm = event.target;
+    let ingrPara = elm.previousElementSibling;
+    let ingrHeading = ingrPara.previousElementSibling;
+    let recipeName = ingrHeading.previousElementSibling;
+    let cardDiv = elm.parentElement;
+    let recipeId = cardDiv.getAttribute('value');
+    document.querySelector('.search-title').style.display = 'none';
+    const returnButton = document.getElementById('back-btn');
+    returnButton.style.display = 'block';
+
+    returnButton.addEventListener('click', () => {
+        cardBox.style.display = 'flex';
+        returnButton.style.display = 'none';
+        document.querySelector('.recipe-details').style.display = 'none';
+        document.querySelector('#recipe-info-title').style.display = 'none'
+        document.querySelector('.search-title').style.display = 'block';
+    })
+
+    fetch(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`)
+    .then(response => handleError(response))
+    .then(data => {
+        document.querySelector('.recipe-details').style.display = 'block';
+        document.querySelector('.card-container').style.display = 'none';
+        document.getElementById('summary').innerHTML = data.summary;
+        document.querySelector('.recipe-details img').setAttribute('src',data.image);
+        document.getElementById('instructions').innerHTML = data.instructions;
+        document.getElementById('ingredients').innerText = ingrPara.innerText;
+        document.querySelector('#recipe-info-title').style.display = 'block';
+        document.querySelector('#recipe-info-title').innerText = recipeName.innerText;
+    })
+}
 
 function handleError(apiResponse) {
     if (apiResponse.ok) {
@@ -200,4 +172,13 @@ function handleError(apiResponse) {
     } else {
         document.getElementById('error').innerText = 'Failed to retrieve complete data';
     }
+    return apiResponse.json();
+}
+
+function getIngredients (ingredientsArray) {
+    let ingredientsName = [];
+    ingredientsArray.forEach(element => {
+        ingredientsName.push(element.name);
+    })
+    return ingredientsName;
 }
